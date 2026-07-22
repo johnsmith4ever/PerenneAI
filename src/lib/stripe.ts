6bec5a0f43a7483846314848486501eb69126fb6
Kyrus <kyrus@Kyruss-Mac-mini.local>
2026-07-22 16:55:31 +0100

    fix stripe api version

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-04-10",
+  apiVersion: "2026-06-24.dahlia",
   appInfo: {
     name: "AI App",
     version: "0.1.0",
